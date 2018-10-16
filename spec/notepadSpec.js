e8f0cc084bdb55850a40874812075c94320ea137
Ibra <ibra@MacBook-Pro-6.local>
2018-10-16 17:06:12 +0100

    committing addNote function, test passing

--- spec/notepadSpec.js
+++ spec/notepadSpec.js
@@ -1,7 +1,15 @@
 (function hasNotesArray() {
     let notepad = new Notepad();
-    console.log(notepad.notes);
     check('Notepad initialized with empty notes array', () => {
         assert.isEmpty(notepad.notes);
+    })
+})();
+
+(function addNote() {
+    let notepad = new Notepad();
+    let note = new Note('hello world');
+    notepad.addNote(note);
+    check('adding note pushes to the array', () => {
+        assert.include(notepad.notes, note);
     })
 })();
